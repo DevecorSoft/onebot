d33448c97ccb25727a6b122b4ee5578b9d4026fa
zhengfeng <zhengfeng.cai@thoughtworks.com>
2023-02-07 00:01:19 +0800

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,15 @@ const test_globals = require('./test/global')
 module.exports = {
   preset: 'ts-jest',
   globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
     ...test_globals
   },
   setupFilesAfterEnv: ['jest-extended'],
   moduleFileExtensions: ['ts', 'js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '\\.ts$': ['ts-jest'],
   },
   testMatch: ['<rootDir>/test/**/*.test.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
