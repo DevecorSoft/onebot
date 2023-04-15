09939de1c72b928807a9ab5c859d79fa282babea
zhengfeng <zhengfeng.cai@thoughtworks.com>
2023-04-15 10:33:45 +0800

    quick fix

--- src/feishu/server.ts
+++ src/feishu/server.ts
@@ -1,2 +1,5 @@
 import { app } from './app'
+import { storage } from '@/feishu/persistentce'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+var PropertiesService = storage
 app.listen(3000)
