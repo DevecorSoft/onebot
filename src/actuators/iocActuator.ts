c2308034285e84b69d4edfbed7a0a0305e2a3ef3
zhengfeng <zhengfeng.cai@thoughtworks.com>
2023-04-15 14:58:31 +0800

    mark actuator as deprecated

--- src/actuators/iocActuator.ts
+++ src/actuators/iocActuator.ts
@@ -1,12 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type AnyFunction = (...args: any) => any
 
+/**
+ * @deprecated
+ */
 export type Actable<D, F extends AnyFunction> = (this: D, ...args: Parameters<F>) => ReturnType<F>
 
+/**
+ * @deprecated
+ */
 export interface Actuator<F> {
   readonly act: F
 }
 
+/**
+ * @deprecated
+ * @param {Function} func func
+ * @param {Object} deps deps
+ * @returns {Object} obj
+ */
 export const createActuator = <D, F extends AnyFunction>(func: Actable<D, F>, deps: D): Actuator<F> & D => ({
   act: func as F,
   ...deps,
