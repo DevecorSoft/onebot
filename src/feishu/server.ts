d2d78367cff6313f71da8c57506cec128322051a
zhengfeng <zhengfeng.cai@thoughtworks.com>
2023-04-15 11:21:55 +0800

    quick fix

--- src/feishu/server.ts
+++ src/feishu/server.ts
@@ -1,5 +1,20 @@
-import { app } from './app'
 import { storage } from '@/feishu/persistentce'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-var
-var PropertiesService = storage
-app.listen(3000)
+import { randomUUID } from 'crypto'
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+global.PropertiesService = storage
+// @ts-ignore
+global.Utilities = {
+  getUuid: randomUUID()
+}
+// @ts-ignore
+global.OAuth2 = {}
+// @ts-ignore
+global.UrlFetchApp = {}
+// @ts-ignore
+global.ServiceAccountCredentials = {}
+/* eslint-enable */
+
+import('./app')
+  .then(({app}) => app.listen(3000))
+  .catch((e) => console.error('unable to start server!\n', e))
